a57ee1f001a8d8e3c6fb633fd508aeb88ab4b884
katerynapidhorna <katerynapidhorna@gmail.com>
2020-06-04 14:23:45 +0200

    add npm package "to-title-case" and change index.js title

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hey you! I know it is challenging, but you can do it!Go for it!')
+const title = require('to-title-case');
+const titleHeading = 'hey you! i know it is challenging, but you can do it!go for it!';
+
+console.log(title(titleHeading))
